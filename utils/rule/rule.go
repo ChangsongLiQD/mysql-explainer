503cc0b707af4eda583298a7384124451cdcfdf4
Jason li <changsongliqd@gmail.com>
2020-04-06 17:33:52 +0800

    add sql reading and rule reading logic

---
+++ utils/rule/rule.go
@@ -0,0 +1,25 @@
+package rule
+
+import "mysql-explainer/explainer"
+
+func getRule(ruleConfig map[string] []string) explainer.Rule{
+	rule := explainer.NewRule()
+	for t, ruleNames := range ruleConfig {
+		tp := getTypeNum(t)
+		for _, name := range ruleNames {
+			rule.AddRule(tp, name, false)
+		}
+	}
+	return rule
+}
+
+func getTypeNum(t string) int{
+	switch t {
+	case "selecttype":
+		return explainer.RULE_SELECT_TYPE
+	case "type":
+		return explainer.RULE_TYPE
+	default:
+		return explainer.RULE_EXTRA
+	}
+}
---
+++ utils/sql/reader.go
@@ -0,0 +1,39 @@
+package sql
+
+import (
+	"bufio"
+	"mysql-explainer/utils/log"
+	"os"
+	"strings"
+)
+
+var (
+	logger = log.GetLogger()
+	buf *bufio.Reader
+	file *os.File
+)
+
+func Init(f string){
+	var err error
+	file, err = os.Open(f)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	buf= bufio.NewReader(file)
+}
+
+func ReadLine() (string, error){
+	sql, err := buf.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+	sql = strings.TrimSpace(sql)
+	return sql, nil
+}
+
+func Close() {
+	err := file.Close()
+	if err != nil {
+		logger.Fatal(err)
+	}
+}
